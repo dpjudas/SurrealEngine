10a6e30e591bfc8860cec743eca2995c4731e809
Magnus Norddahl <dpjudas@users.noreply.github.com>
2021-08-11 19:46:29 +0200

    Implement NavigationPoint.describeSpec

--- UTEngine/Native/NNavigationPoint.cpp
+++ UTEngine/Native/NNavigationPoint.cpp
@@ -2,6 +2,8 @@
 #include "Precomp.h"
 #include "NNavigationPoint.h"
 #include "VM/NativeFunc.h"
+#include "UObject/UActor.h"
+#include "UObject/ULevel.h"
 
 void NNavigationPoint::RegisterFunctions()
 {
@@ -10,5 +12,21 @@ void NNavigationPoint::RegisterFunctions()
 
 void NNavigationPoint::describeSpec(UObject* Self, int iSpec, UObject*& Start, UObject*& End, int& ReachFlags, int& Distance)
 {
-	throw std::runtime_error("NavigationPoint.describeSpec not implemented");
+	UActor* SelfActor = UObject::Cast<UActor>(Self);
+	ULevel* level = SelfActor->XLevel();
+	if (iSpec >= 0 && (size_t)iSpec < level->ReachSpecs.size())
+	{
+		auto& spec = level->ReachSpecs[iSpec];
+		Start = (spec.startActor >= 0 && (size_t)spec.startActor < level->Actors.size()) ? level->Actors[spec.startActor] : nullptr;
+		End = (spec.endActor >= 0 && (size_t)spec.endActor < level->Actors.size()) ? level->Actors[spec.endActor] : nullptr;
+		ReachFlags = spec.reachFlags;
+		Distance = spec.distance;
+	}
+	else
+	{
+		Start = nullptr;
+		End = nullptr;
+		ReachFlags = 0;
+		Distance = 0;
+	}
 }
