020c6baafb2c397260593afce4a8c0ee1427b842
Magnus Norddahl <dpjudas@users.noreply.github.com>
2021-09-19 05:34:12 +0200

    More rand fixes as it seems RAND_MAX is actually 2^32-1 on linux (on Windows it is only 2^16-1)

--- UTEngine/UObject/UTexture.cpp
+++ UTEngine/UObject/UTexture.cpp
@@ -231,8 +231,8 @@ void UFireTexture::UpdateFrame()
 						else if (y >= height) y -= height;
 						pixels[x + y * width] = c;
 
-						x0 += rand() * 2.0f / (float)RAND_MAX - 1.0f;
-						y0 += rand() * 2.0f / (float)RAND_MAX - 1.0f;
+						x0 += (float)(rand() * 2.0 / RAND_MAX - 1.0);
+						y0 += (float)(rand() * 2.0 / RAND_MAX - 1.0);
 					}
 				}
 				break;
