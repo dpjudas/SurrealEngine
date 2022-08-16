32b7c1c66de945a4b7562347b4bd836b26462831
Magnus Norddahl <dpjudas@users.noreply.github.com>
2022-08-16 15:09:03 +0200

    Add missing include

--- UTEngine/Window/X11/X11Window.cpp
+++ UTEngine/Window/X11/X11Window.cpp
@@ -23,6 +23,7 @@
 #include "X11Window.h"
 #include "RenderDevice/RenderDevice.h"
 #include "RenderDevice/Vulkan/VulkanDevice.h"
+#include "RenderDevice/Vulkan/VulkanSurface.h"
 #include "RenderDevice/Vulkan/VulkanCompatibleDevice.h"
 #include "Engine.h"
 #include <dlfcn.h>
