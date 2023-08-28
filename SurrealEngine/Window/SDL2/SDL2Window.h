0444df827f865641b400645cbee85aab665571a7
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2023-08-28 21:33:56 +0300

    SDL2Window: Text formatting fixes

--- SurrealEngine/Window/SDL2/SDL2Window.h
+++ SurrealEngine/Window/SDL2/SDL2Window.h
@@ -4,56 +4,57 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
+
 #include "Window/Window.h"
 
 class SDL2Window : public DisplayWindow
 {
-    SDL_Window* m_SDLWindow;
+	SDL_Window* m_SDLWindow;
 public:
-    SDL2Window(DisplayWindowHost *windowHost);
-    ~SDL2Window();
+	SDL2Window(DisplayWindowHost *windowHost);
+	~SDL2Window();
 
-    static void ProcessEvents();
+	static void ProcessEvents();
 	static void RunLoop();
 	static void ExitLoop();
 
 	void OnSDLEvent(SDL_Event& event);
 
-    void SetWindowTitle(const std::string& text) override;
-    void SetWindowFrame(const Rect& box) override;
+	void SetWindowTitle(const std::string& text) override;
+	void SetWindowFrame(const Rect& box) override;
 	void SetClientFrame(const Rect& box) override;
-    void Show() override;
-    void ShowFullscreen() override;
-    void ShowMaximized() override;
-    void ShowMinimized() override;
-    void ShowNormal() override;
-    void Hide() override;
-    void Activate() override;
-    void ShowCursor(bool enable) override;
-    void LockCursor() override;
+	void Show() override;
+	void ShowFullscreen() override;
+	void ShowMaximized() override;
+	void ShowMinimized() override;
+	void ShowNormal() override;
+	void Hide() override;
+	void Activate() override;
+	void ShowCursor(bool enable) override;
+	void LockCursor() override;
 	void UnlockCursor() override;
 	void Update() override;
 	bool GetKeyState(EInputKey key) override;
 
-    RenderDevice* GetRenderDevice() override { return rendDevice.get(); }
+	RenderDevice* GetRenderDevice() override { return rendDevice.get(); }
 
-    void OnKeyboardInput(SDL_KeyboardEvent& event);
-    void OnKeyboardTextInput(SDL_TextInputEvent& event);
-    void OnMouseInput(SDL_MouseButtonEvent& event);
-    void OnMouseWheel(SDL_MouseWheelEvent& event);
-    void OnMouseMove(SDL_MouseMotionEvent& event);
+	void OnKeyboardInput(SDL_KeyboardEvent& event);
+	void OnKeyboardTextInput(SDL_TextInputEvent& event);
+	void OnMouseInput(SDL_MouseButtonEvent& event);
+	void OnMouseWheel(SDL_MouseWheelEvent& event);
+	void OnMouseMove(SDL_MouseMotionEvent& event);
 
-    Rect GetWindowFrame() const override;
+	Rect GetWindowFrame() const override;
 	Size GetClientSize() const override;
 	int GetPixelWidth() const override;
 	int GetPixelHeight() const override;
 	double GetDpiScale() const override;
 
-    EInputKey SDLScancodeToInputKey(SDL_Scancode keycode);
-    SDL_Scancode InputKeyToSDLScancode(EInputKey inputkey);
+	EInputKey SDLScancodeToInputKey(SDL_Scancode keycode);
+	SDL_Scancode InputKeyToSDLScancode(EInputKey inputkey);
 
-    DisplayWindowHost* windowHost = nullptr;
-    std::unique_ptr<RenderDevice> rendDevice;
+	DisplayWindowHost* windowHost = nullptr;
+	std::unique_ptr<RenderDevice> rendDevice;
 
-    static std::map<int, SDL2Window*> windows;
+	static std::map<int, SDL2Window*> windows;
 };
