b6840527dac580adc6359d8a92aae67f7a4f1ac9
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2023-09-10 18:18:49 +0300

    SDL2Window: Introduce SDLWindowError()

--- SurrealEngine/Window/SDL2/SDL2Window.h
+++ SurrealEngine/Window/SDL2/SDL2Window.h
@@ -18,6 +18,7 @@ public:
 	static void RunLoop();
 	static void ExitLoop();
 
+	void SDLWindowError(std::string& message) const;
 	void OnSDLEvent(SDL_Event& event);
 
 	void SetWindowTitle(const std::string& text) override;
