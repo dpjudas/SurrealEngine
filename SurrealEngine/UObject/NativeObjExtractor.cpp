a931d26925cbfcda73ca17e2da05b978b1d0a6a0
Xaleros <xaleros99@gmail.com>
2023-12-22 22:44:51 -0600

    NativeObjExtractor outputs json files instead of text files

--- SurrealEngine/UObject/NativeObjExtractor.cpp
+++ SurrealEngine/UObject/NativeObjExtractor.cpp
@@ -10,164 +10,81 @@
 
 std::string NativeObjExtractor::Run(PackageManager* packages)
 {
-	std::string structs;
-	std::string api;
-	std::string inits;
-	std::string pkginits;
+	JsonValue jsonRoot = JsonValue::object();
 
-	pkginits += "void InitPropertyOffsets(PackageManager* packages)\r\n{\r\n";
-
-	for (std::string pkgname : { "Core", "Engine", "Fire", "IpDrv", "DeusEx" })
+	for (NameString pkgname : packages->GetPackageNames())
 	{
-		Package* package = packages->GetPackage(pkgname);
-		std::vector<UClass*> classes = package->GetAllClasses();
+		if (pkgname == "Editor")
+			continue;
 
-		for (UClass* cls : classes)
+		Package* package = packages->GetPackage(pkgname.ToString());
+
+		JsonValue jsonPackage = CreatePackageJson(package);
+		if (jsonPackage.properties().size() > 0)
 		{
-			api += WriteClassHeader(cls);
-			structs += WriteStructHeader(cls);
-			inits += WriteStructBody(package, cls);
+			jsonRoot.add(pkgname.ToString(), jsonPackage);
 		}
+	}
 
-		for (UClass* cls : classes)
+	return jsonRoot.to_json();
+}
+
+JsonValue NativeObjExtractor::CreatePackageJson(Package* package)
+{
+	JsonValue jsonPackage = JsonValue::object();
+
+	std::vector<UClass*> classes = package->GetAllClasses();
+	for (UClass* cls : classes)
+	{
+		if (AllFlags(cls->Flags, ObjectFlags::Native))
 		{
-			bool propFound = false;
-			for (UField* child = cls->Children; child != nullptr; child = child->Next)
+			JsonValue jsonClass = CreateClassJson(cls);
+			if (jsonClass.properties().size() > 0)
 			{
-				UProperty* prop = UClass::TryCast<UProperty>(child);
-				if (prop)
-				{
-					propFound = true;
-					break;
-				}
+				jsonPackage.add(cls->Name.ToString(), jsonClass);
 			}
-
-			if (propFound)
-				pkginits += "\tInitPropertyOffsets_" + cls->Name.ToString() + "(packages);\r\n";
 		}
-
 	}
 
-	pkginits += "}\r\n\r\n";
-
-	return api + "\r\n\r\n////////////////\r\n\r\n" + structs + "\r\n\r\n////////////////\r\n\r\n" + inits + pkginits;
+	return jsonPackage;
 }
 
-std::string NativeObjExtractor::WriteStructHeader(UClass* cls)
+JsonValue NativeObjExtractor::CreateClassJson(UClass* cls)
 {
-	std::map<NameString, std::string> locations;
-
+	JsonValue jsonClass = JsonValue::object();
 	for (UField* child = cls->Children; child != nullptr; child = child->Next)
 	{
 		UProperty* prop = UClass::TryCast<UProperty>(child);
 		if (prop)
 		{
-			locations[prop->Name] = "\tsize_t " + prop->Name.ToString() + ";\r\n";
+			std::string type;
+			std::string comment;
+
+			if (UClass::TryCast<UIntProperty>(prop)) type = "int";
+			else if (UClass::TryCast<UByteProperty>(prop)) type = "uint8_t";
+			else if (UClass::TryCast<UBoolProperty>(prop)) type = "BitfieldBool";
+			else if (UClass::TryCast<UObjectProperty>(prop)) 
+			{ 
+				type = "UObject*"; 
+				auto objprop = UClass::Cast<UObjectProperty>(prop); 
+				if (objprop->ObjectClass && objprop->ObjectClass->Name != "Object") 
+					type = "U" + objprop->ObjectClass->Name.ToString() + "*"; 
+			}
+			else if (UClass::TryCast<UFloatProperty>(prop)) type = "float";
+			else if (UClass::TryCast<UFixedArrayProperty>(prop)) type = "std::vector<void*>";
+			else if (UClass::TryCast<UArrayProperty>(prop)) type = "std::vector<void*>";
+			else if (UClass::TryCast<UMapProperty>(prop)) type = "std::map<void*, void*>*";
+			else if (UClass::TryCast<UClassProperty>(prop)) type = "UClass*";
+			else if (UClass::TryCast<UStructProperty>(prop)) type = UClass::Cast<UStructProperty>(prop)->Struct->Name.ToString();
+			else if (UClass::TryCast<UNameProperty>(prop)) type = "NameString";
+			else if (UClass::TryCast<UStrProperty>(prop)) type = "std::string";
+			else if (UClass::TryCast<UStringProperty>(prop)) type = "std::string";
+
+			if (NameString(type) == "Vector")
+				type = "vec3";
+
+			jsonClass.add(prop->Name.ToString(), JsonValue::string(type));
 		}
 	}
-	if (locations.empty())
-		return {};
-
-	std::string header;
-	header += "struct PropertyOffsets_" + cls->Name.ToString() + "\r\n";
-	header += "{\r\n";
-	for (auto& it : locations)
-		header += it.second;
-	header += "};\r\n\r\n";
-	header += "extern PropertyOffsets_" + cls->Name.ToString() + " PropOffsets_" + cls->Name.ToString() + ";\r\n\r\n";
-	return header;
-}
-
-std::string NativeObjExtractor::WriteClassHeader(UClass* cls)
-{
-	std::map<NameString, std::string> props;
-
-	for (UField* child = cls->Children; child != nullptr; child = child->Next)
-	{
-		UProperty* prop = UClass::TryCast<UProperty>(child);
-		if (prop)
-		{
-			props[prop->Name] = "\t" + WritePropertyGetter(cls->Name, prop) + "\r\n";
-		}
-	}
-	if (props.empty())
-		return {};
-
-	std::string header;
-	header += "\r\nclass U" + cls->Name.ToString() + "\r\n";
-	header += "{\r\npublic:\r\n";
-	for (auto& it : props)
-		header += it.second;
-	header += "};\r\n";
-	return header;
-}
-
-std::string NativeObjExtractor::WriteStructBody(Package* package, UClass* cls)
-{
-	std::map<NameString, std::string> props;
-
-	for (UField* child = cls->Children; child != nullptr; child = child->Next)
-	{
-		UProperty* prop = UClass::TryCast<UProperty>(child);
-		if (prop)
-		{
-			props[prop->Name] = "\tPropOffsets_" + cls->Name.ToString() + "." + prop->Name.ToString() + " = cls->GetProperty(\"" + prop->Name.ToString() + "\")->DataOffset;\r\n";
-		}
-	}
-	if (props.empty())
-		return {};
-
-	std::string body;
-
-	body += "PropertyOffsets_" + cls->Name.ToString() + " PropOffsets_" + cls->Name.ToString() + ";\r\n\r\n";
-	body += "static void InitPropertyOffsets_" + cls->Name.ToString() + "(PackageManager* packages)\r\n{\r\n";
-	body += "\tUClass* cls = dynamic_cast<UClass*>(packages->GetPackage(\"" + package->GetPackageName().ToString() + "\")->GetUObject(\"Class\", \"" + cls->Name.ToString() + "\"));\r\n";
-	body += "\tif (!cls)\r\n";
-	body += "\t{\r\n";
-	body += "\t\tmemset(&PropOffsets_" + cls->Name.ToString() + ", 0xff, sizeof(PropOffsets_" + cls->Name.ToString() + ")); \r\n";
-	body += "\t\treturn;\r\n";
-	body += "\t}\r\n";
-
-	for (auto& it : props)
-		body += it.second;
-	body += "}\r\n\r\n";
-
-	return body;
-}
-
-std::string NativeObjExtractor::WritePropertyGetter(const NameString& clsname, UProperty* prop)
-{
-	std::string type;
-	std::string comment;
-
-	if (UClass::TryCast<UIntProperty>(prop)) type = "int";
-	else if (UClass::TryCast<UByteProperty>(prop)) type = "uint8_t";
-	else if (UClass::TryCast<UBoolProperty>(prop)) type = "bool";
-	else if (UClass::TryCast<UObjectProperty>(prop)) { type = "UObject*"; auto objprop = UClass::Cast<UObjectProperty>(prop); if (objprop->ObjectClass && objprop->ObjectClass->Name != "Object") type = "U" + objprop->ObjectClass->Name.ToString() + "*"; }
-	else if (UClass::TryCast<UFloatProperty>(prop)) type = "float";
-	else if (UClass::TryCast<UFixedArrayProperty>(prop)) type = "std::vector<void*>";
-	else if (UClass::TryCast<UArrayProperty>(prop)) type = "std::vector<void*>";
-	else if (UClass::TryCast<UMapProperty>(prop)) type = "std::map<void*, void*>*";
-	else if (UClass::TryCast<UClassProperty>(prop)) type = "UClass*";
-	else if (UClass::TryCast<UStructProperty>(prop)) type = UClass::Cast<UStructProperty>(prop)->Struct->Name.ToString();
-	else if (UClass::TryCast<UNameProperty>(prop)) type = "NameString";
-	else if (UClass::TryCast<UStrProperty>(prop)) type = "std::string";
-	else if (UClass::TryCast<UStringProperty>(prop)) type = "std::string";
-
-	if (NameString(type) == "Vector") type = "vec3";
-
-	if (AllFlags(prop->PropFlags, PropertyFlags::Native))
-	{
-		if (!comment.empty())
-			comment += ", ";
-		comment += "native";
-	}
-
-	if (!comment.empty())
-		comment = " // " + comment;
-
-	if (type == "bool")
-		return "BitfieldBool " + prop->Name.ToString() + "() { return BoolValue(PropOffsets_" + clsname.ToString() + "." + prop->Name.ToString() + "); }" + comment;
-	else
-		return type + "& " + prop->Name.ToString() + "() { return Value<" + type + ">(PropOffsets_" + clsname.ToString() + "." + prop->Name.ToString() + "); }" + comment;
+	return jsonClass;
 }
