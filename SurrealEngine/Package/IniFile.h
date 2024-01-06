30f0f40f33a0ffe0687d92275d9dd9d2f3e752ef
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-01-06 23:23:35 +0300

    IniFile: Allow writing to a different file than the opened one

--- SurrealEngine/Package/IniFile.h
+++ SurrealEngine/Package/IniFile.h
@@ -17,11 +17,16 @@ public:
 	void SetValue(NameString sectionName, NameString keyName, const std::string& newValue);
 	void SetValues(NameString sectionName, NameString keyName, const std::vector<std::string>& newValues);
 
-	void SaveTo(const std::string& filename);
+	// Saves values to the ini file the said values are loaded from
+	void SaveTo();
+	// Saves values to a specified file
+	void SaveTo(const std::string& filename); 
 
 private:
 	bool ReadLine(const std::string& text, size_t& pos, std::string& line);
 
 	bool isModified = false;
+
+	std::string ini_file_path;
 	std::map<NameString, std::map<NameString, std::vector<std::string>>> sections;
 };
