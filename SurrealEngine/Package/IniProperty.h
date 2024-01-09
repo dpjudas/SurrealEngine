f5fb01a0e20cb534de9519aeed3bd3dc3417eb5e
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-01-10 02:44:38 +0300

    Move definitions to IniProperty.cpp so GCC HOPEFULLY stops complaining.

--- SurrealEngine/Package/IniProperty.h
+++ SurrealEngine/Package/IniProperty.h
@@ -9,22 +9,10 @@ template <typename T>
 class IniPropertyConverter
 {
 public:
-	static std::string ToString(const T& value) { return std::to_string(value); }
+	static std::string ToString(const T& value);
 	static T FromString(const std::string& valueString);
 	static T FromString(const char* valueString) { return FromString(std::string(valueString)); }
 	static T FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const T& default_value);
 
 	IniPropertyConverter() = delete;
 };
-
-template<>
-std::string IniPropertyConverter<AudioFrequency>::ToString(const AudioFrequency& value)
-{
-	return std::to_string(value.frequency) + "Hz";
-}
-
-template<>
-std::string IniPropertyConverter<bool>::ToString(const bool& value)
-{
-	return value ? "True" : "False";
-}
