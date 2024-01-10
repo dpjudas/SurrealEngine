bfcdd734f5d1dd56b95ddc92a3938fec3b2f19c6
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-01-10 21:36:26 +0300

    IniProperty.cpp: Always implement ToString() before FromIniFile(), which calls the former

--- SurrealEngine/Package/IniProperty.cpp
+++ SurrealEngine/Package/IniProperty.cpp
@@ -8,15 +8,15 @@ int IniPropertyConverter<int>::FromString(const std::string& valueString)
 }
 
 template<>
-int IniPropertyConverter<int>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const int& default_value)
-{
-	return std::stoi(iniFile.GetValue(section, keyName, ToString(default_value)));
-}
-
-template<>
 std::string IniPropertyConverter<int>::ToString(const int& value)
 {
 	return std::to_string(value);
+}
+
+template<>
+int IniPropertyConverter<int>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const int& default_value)
+{
+	return std::stoi(iniFile.GetValue(section, keyName, ToString(default_value)));
 }
 
 //====================================================================
@@ -28,15 +28,15 @@ float IniPropertyConverter<float>::FromString(const std::string& valueString)
 }
 
 template<>
-float IniPropertyConverter<float>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const float& default_value)
-{
-	return std::stof(iniFile.GetValue(section, keyName, ToString(default_value)));
-}
-
-template<>
 std::string IniPropertyConverter<float>::ToString(const float& value)
 {
 	return std::to_string(value);
+}
+
+template<>
+float IniPropertyConverter<float>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const float& default_value)
+{
+	return std::stof(iniFile.GetValue(section, keyName, ToString(default_value)));
 }
 
 //====================================================================
@@ -48,15 +48,15 @@ uint8_t IniPropertyConverter<uint8_t>::FromString(const std::string& valueString
 }
 
 template<>
-uint8_t IniPropertyConverter<uint8_t>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const uint8_t& default_value)
-{
-	return std::stoul(iniFile.GetValue(section, keyName, ToString(default_value)));
-}
-
-template<>
 std::string IniPropertyConverter<uint8_t>::ToString(const uint8_t& value)
 {
 	return std::to_string(value);
+}
+
+template<>
+uint8_t IniPropertyConverter<uint8_t>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const uint8_t& default_value)
+{
+	return std::stoul(iniFile.GetValue(section, keyName, ToString(default_value)));
 }
 
 //====================================================================
@@ -69,19 +69,20 @@ bool IniPropertyConverter<bool>::FromString(const std::string& valueString)
 	else if (valueString == "False" || valueString == "false" || valueString == "0")
 		return false;
 	else
-		throw std::runtime_error("Encountered a non-boolean value: " + valueString);
+		throw std::invalid_argument("Encountered a non-boolean value: " + valueString);
+}
+
+
+template<>
+std::string IniPropertyConverter<bool>::ToString(const bool& value)
+{
+	return value ? "True" : "False";
 }
 
 template<>
 bool IniPropertyConverter<bool>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const bool& default_value)
 {
 	return FromString(iniFile.GetValue(section, keyName, ToString(default_value)));
-}
-
-template<>
-std::string IniPropertyConverter<bool>::ToString(const bool& value)
-{
-	return value ? "True" : "False";
 }
 
 //====================================================================
@@ -93,15 +94,15 @@ std::string IniPropertyConverter<std::string>::FromString(const std::string& val
 }
 
 template<>
-std::string IniPropertyConverter<std::string>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const std::string& default_value)
-{
-	return iniFile.GetValue(section, keyName, default_value);
-}
-
-template<>
 std::string IniPropertyConverter<std::string>::ToString(const std::string& value)
 {
 	return value;
+}
+
+template<>
+std::string IniPropertyConverter<std::string>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const std::string& default_value)
+{
+	return iniFile.GetValue(section, keyName, default_value);
 }
 
 //====================================================================
@@ -114,17 +115,17 @@ AudioFrequency IniPropertyConverter<AudioFrequency>::FromString(const std::strin
 		int frequency = std::stoi(valueString.substr(0, pos));
 		return AudioFrequency(frequency);
 	}
-	throw std::runtime_error("Invalid value " + valueString + " for changing frequency.");
-}
-
-template<>
-AudioFrequency IniPropertyConverter<AudioFrequency>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const AudioFrequency& default_value)
-{
-	return FromString(iniFile.GetValue(section, keyName, ToString(default_value)));
+	throw std::invalid_argument("Invalid value '" + valueString + "' for changing frequency. The value must be a number that ends with 'Hz'.");
 }
 
 template<>
 std::string IniPropertyConverter<AudioFrequency>::ToString(const AudioFrequency& value)
 {
 	return std::to_string(value.frequency) + "Hz";
+}
+
+template<>
+AudioFrequency IniPropertyConverter<AudioFrequency>::FromIniFile(const IniFile& iniFile, const NameString& section, const NameString& keyName, const AudioFrequency& default_value)
+{
+	return FromString(iniFile.GetValue(section, keyName, ToString(default_value)));
 }
