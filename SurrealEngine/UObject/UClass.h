60e491192eab3794d2ac63b10375aab2c1538113
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-01-15 01:08:06 +0300

    Modify the signatures of ClassFlags operators

--- SurrealEngine/UObject/UClass.h
+++ SurrealEngine/UObject/UClass.h
@@ -170,8 +170,9 @@ enum class ClassFlags : uint32_t
 	NativeReplication = 0x00800, // Replication handled in C++
 };
 
-static const uint32_t operator&(ClassFlags lhs, ClassFlags rhs) { return uint32_t(lhs) & uint32_t(rhs); }
-static const uint32_t operator|(ClassFlags lhs, ClassFlags rhs) { return uint32_t(lhs) | uint32_t(rhs); }
+static uint32_t operator&(const ClassFlags lhs, const ClassFlags rhs) { return uint32_t(lhs) & uint32_t(rhs); }
+static uint32_t operator|(const ClassFlags lhs, const ClassFlags rhs) { return uint32_t(lhs) | uint32_t(rhs); }
+static uint32_t operator^(const ClassFlags lhs, const ClassFlags rhs) { return uint32_t(lhs) ^ uint32_t(rhs); }
 
 class UClass : public UState
 {
