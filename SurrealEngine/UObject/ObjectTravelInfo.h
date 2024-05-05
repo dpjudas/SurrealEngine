72703b99601632972ba644e004d4a35ab8874644
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2024-05-05 17:13:07 +0300

    ObjectTravelInfo: Parse PlayerPawn fields too

--- SurrealEngine/UObject/ObjectTravelInfo.h
+++ SurrealEngine/UObject/ObjectTravelInfo.h
@@ -4,16 +4,20 @@
 
 class UProperty;
 class UInventory;
+class UActor;
 
-// Travel info parser for a single UInventory
+// Travel info parser for UInventory and UPlayerPawn-derived classes
 struct ObjectTravelInfo
 {
-	ObjectTravelInfo(UInventory* inventory);
+	//ObjectTravelInfo(UInventory* inventory);
+	ObjectTravelInfo() {}
+	ObjectTravelInfo(UActor* travelActor);
 	static std::vector<ObjectTravelInfo> Parse(const std::string& text);
 	static std::string ToString(const std::vector<ObjectTravelInfo>& objects);
 
-	std::string ClassName;
-	std::map<std::string, std::string> Properties;
+	std::string ClassName; // Full class name of the traveling actor
+	bool isPlayerPawn = false; // true = Actor is a PlayerPawn, false = Actor is an Inventory
+	std::map<std::string, std::string> Properties; // Properties to travel
 };
 
 static ObjectTravelInfo ParseSingleObject(const std::string& singleObjectText);
