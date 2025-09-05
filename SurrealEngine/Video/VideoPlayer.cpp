47ac21da65d10bf7886f181692dc2863b87b4e0d
Magnus Norddahl <dpjudas@users.noreply.github.com>
2025-09-05 06:02:05 +0200

    Clean up VideoPlayer a bit

--- SurrealEngine/Video/VideoPlayer.cpp
+++ SurrealEngine/Video/VideoPlayer.cpp
@@ -80,135 +80,7 @@ public:
 	VideoPlayerImpl(const std::string& filename) : Decoder(CreateVideoDecoder(), &ReleaseVideoDecoder)
 	{
 		reader = std::make_unique<AVIFileReader>(File::read_all_bytes(filename));
-
-		reader->PushChunk("RIFF");
-		reader->ReadTag("AVI ");
-
-		// AVI header must be first chunk
-		reader->SkipJunk();
-		reader->PushChunk("LIST");
-		{
-			reader->ReadTag("hdrl");
-
-			reader->PushChunk("avih");
-			if (reader->GetChunkSize() < 56)
-				throw std::runtime_error("Unsupported AVI header size");
-			mainHeader.MicroSecPerFrame = reader->ReadUint32();
-			mainHeader.MaxBytesPerSec = reader->ReadUint32();
-			mainHeader.PaddingGranularity = reader->ReadUint32();
-			mainHeader.Flags = reader->ReadUint32();
-			mainHeader.TotalFrames = reader->ReadUint32();
-			mainHeader.InitialFrames = reader->ReadUint32();
-			mainHeader.Streams = reader->ReadUint32();
-			mainHeader.SuggestedBufferSize = reader->ReadUint32();
-			mainHeader.Width = reader->ReadUint32();
-			mainHeader.Height = reader->ReadUint32();
-			mainHeader.Reserved[0] = reader->ReadUint32();
-			mainHeader.Reserved[1] = reader->ReadUint32();
-			mainHeader.Reserved[2] = reader->ReadUint32();
-			mainHeader.Reserved[3] = reader->ReadUint32();
-			reader->PopChunk();
-
-			reader->SkipJunk();
-			while (!reader->IsEndOfChunk())
-			{
-				reader->PushChunk("LIST");
-				reader->ReadTag("strl");
-
-				AVIStreamHeader streamHeader;
-				reader->PushChunk("strh");
-				if (reader->GetChunkSize() < 56)
-					throw std::runtime_error("Unsupported AVI stream header size");
-				streamHeader.StreamType = reader->ReadTag();
-				streamHeader.Codec = reader->ReadTag();
-				streamHeader.Flags = reader->ReadUint32();
-				streamHeader.Priority = reader->ReadUint16();
-				streamHeader.Language = reader->ReadUint16();
-				streamHeader.InitialFrames = reader->ReadUint32(); // How far ahead the audio compared to the video
-				streamHeader.Scale = reader->ReadUint32();
-				streamHeader.Rate = reader->ReadUint32(); // Rate / Scale = samples per second
-				streamHeader.Start = reader->ReadUint32(); // Start time
-				streamHeader.Length = reader->ReadUint32(); // Length of video
-				streamHeader.SuggestedBufferSize = reader->ReadUint32();
-				streamHeader.Quality = reader->ReadUint32();
-				streamHeader.SampleSize = reader->ReadUint32(); // Block align for audio, zero if varying
-				streamHeader.Frame.Left = reader->ReadInt16();
-				streamHeader.Frame.Top = reader->ReadInt16();
-				streamHeader.Frame.Right = reader->ReadInt16();
-				streamHeader.Frame.Bottom = reader->ReadInt16();
-				reader->PopChunk();
-
-				reader->PushChunk("strf");
-				if (streamHeader.StreamType == "vids")
-				{
-					if (reader->GetChunkSize() < 40)
-						throw std::runtime_error("Unsupported AVI video stream header size");
-					int size = reader->ReadUint32(); // cbSize in BITMAPINFOHEADER
-					if (size < 40)
-						throw std::runtime_error("Unsupported AVI video stream header size");
-					streamHeader.Video.Width = reader->ReadUint32();
-					streamHeader.Video.Height = reader->ReadUint32();
-					streamHeader.Video.Planes = reader->ReadUint16();
-					streamHeader.Video.BitCount = reader->ReadUint16();
-					streamHeader.Video.Compression = reader->ReadUint32();
-					streamHeader.Video.ImageSize = reader->ReadUint32();
-					streamHeader.Video.XPixelsPerMeter = reader->ReadUint32();
-					streamHeader.Video.YPixelsPerMeter = reader->ReadUint32();
-					streamHeader.Video.ColorsUsed = reader->ReadUint32();
-					streamHeader.Video.ColorsImportant = reader->ReadUint32();
-
-				}
-				else if (streamHeader.StreamType == "auds")
-				{
-					if (reader->GetChunkSize() < 18)
-						throw std::runtime_error("Unsupported AVI audio stream header size");
-					streamHeader.Audio.FormatTag = reader->ReadUint16();
-					streamHeader.Audio.Channels = reader->ReadUint16();
-					streamHeader.Audio.SamplesPerSec = reader->ReadUint32();
-					streamHeader.Audio.AvgSamplesPerSec = reader->ReadUint32();
-					streamHeader.Audio.BlockAlign = reader->ReadUint16();
-					streamHeader.Audio.BitsPerSample = reader->ReadUint16();
-					streamHeader.Audio.SizeExtra = reader->ReadUint16();
-					if (streamHeader.Audio.FormatTag == 2 /*WAVE_FORMAT_ADPCM*/ && streamHeader.Audio.SizeExtra == 32)
-					{
-						// This nasty thing:
-						// https://wiki.multimedia.cx/index.php/Microsoft_ADPCM
-					}
-					else if (streamHeader.Audio.FormatTag == 0xfffe /*WAVE_FORMAT_EXTENSIBLE*/ && streamHeader.Audio.SizeExtra >= 22)
-					{
-						streamHeader.Audio.SamplesPerBlock = reader->ReadUint16();
-						streamHeader.Audio.ChannelMask = reader->ReadUint32();
-						reader->Read(streamHeader.Audio.SubFormat, 16);
-					}
-				}
-				reader->PopChunk();
-
-				/*
-				if (!reader->IsEndOfChunk())
-				{
-					std::string tag = reader->ReadTag();
-					//reader->ReadTag("strd");
-					// additional header data
-				}
-				if (!reader->IsEndOfChunk())
-				{
-					reader->ReadTag("strn");
-					// stream name
-				}
-				*/
-
-				streamHeaders.push_back(streamHeader);
-				reader->PopChunk();
-				reader->SkipJunk();
-			}
-		}
-		reader->PopChunk();
-
-		// movi list must be second chunk
-		reader->SkipJunk();
-		reader->PushChunk("LIST");
-		reader->ReadTag("movi");
-		reader->SkipJunk();
+		ReadHeaders();
 	}
 
 	bool Decode() override
@@ -223,46 +95,37 @@ public:
 				while (!reader->IsEndOfChunk())
 				{
 					type = reader->PushChunk();
-
-					size_t packetSize = reader->GetChunkSize();
-					if (packetData.size() < packetSize)
-						packetData.resize(packetSize);
-					reader->Read(packetData.data(), packetSize);
-
-					if (type.substr(2) == "wb") // Audio frame
-					{
-						DecodeAudio(packetData.data(), packetSize);
-					}
-					else if (type.substr(2) == "dc" || type.substr(2) == "db") // Video frame
-					{
-						DecodeVideo(packetData.data(), packetSize);
-					}
-
+					ReadPacket(type);
 					reader->PopChunk();
 					reader->SkipJunk();
 				}
 			}
 			else
 			{
-				size_t packetSize = reader->GetChunkSize();
-				if (packetData.size() < packetSize)
-					packetData.resize(packetSize);
-				reader->Read(packetData.data(), packetSize);
-
-				if (type.substr(2) == "wb") // Audio frame
-				{
-					DecodeAudio(packetData.data(), packetSize);
-				}
-				else if (type.substr(2) == "dc" || type.substr(2) == "db") // Video frame
-				{
-					DecodeVideo(packetData.data(), packetSize);
-				}
+				ReadPacket(type);
 			}
 			reader->PopChunk();
 			reader->SkipJunk();
 			return true;
 		}
 		return false;
+	}
+
+	void ReadPacket(const std::string& type)
+	{
+		size_t packetSize = reader->GetChunkSize();
+		if (packetData.size() < packetSize)
+			packetData.resize(packetSize);
+		reader->Read(packetData.data(), packetSize);
+
+		if (type.substr(2) == "wb") // Audio frame
+		{
+			DecodeAudio(packetData.data(), packetSize);
+		}
+		else if (type.substr(2) == "dc" || type.substr(2) == "db") // Video frame
+		{
+			DecodeVideo(packetData.data(), packetSize);
+		}
 	}
 
 	UnrealMipmap* NextVideoFrame()
@@ -309,6 +172,140 @@ public:
 			frame->Data.resize(width * height * sizeof(uint32_t));
 			memcpy(frame->Data.data(), pixels, width * height * sizeof(uint32_t));
 			Video.DecodedFrames.push_back(std::move(frame));
+		}
+	}
+
+	void ReadHeaders()
+	{
+		reader->PushChunk("RIFF");
+		reader->ReadTag("AVI ");
+
+		// AVI header must be first chunk
+		reader->SkipJunk();
+		reader->PushChunk("LIST");
+		{
+			reader->ReadTag("hdrl");
+
+			reader->PushChunk("avih");
+			ReadMainHeader();
+			reader->PopChunk();
+
+			reader->SkipJunk();
+			while (!reader->IsEndOfChunk())
+			{
+				reader->PushChunk("LIST");
+				reader->ReadTag("strl");
+				ReadStreamHeader();
+				reader->PopChunk();
+				reader->SkipJunk();
+			}
+		}
+		reader->PopChunk();
+
+		// movi list must be second chunk
+		reader->SkipJunk();
+		reader->PushChunk("LIST");
+		reader->ReadTag("movi");
+		reader->SkipJunk();
+	}
+
+	void ReadMainHeader()
+	{
+		if (reader->GetChunkSize() < 56)
+			throw std::runtime_error("Unsupported AVI header size");
+		mainHeader.MicroSecPerFrame = reader->ReadUint32();
+		mainHeader.MaxBytesPerSec = reader->ReadUint32();
+		mainHeader.PaddingGranularity = reader->ReadUint32();
+		mainHeader.Flags = reader->ReadUint32();
+		mainHeader.TotalFrames = reader->ReadUint32();
+		mainHeader.InitialFrames = reader->ReadUint32();
+		mainHeader.Streams = reader->ReadUint32();
+		mainHeader.SuggestedBufferSize = reader->ReadUint32();
+		mainHeader.Width = reader->ReadUint32();
+		mainHeader.Height = reader->ReadUint32();
+		mainHeader.Reserved[0] = reader->ReadUint32();
+		mainHeader.Reserved[1] = reader->ReadUint32();
+		mainHeader.Reserved[2] = reader->ReadUint32();
+		mainHeader.Reserved[3] = reader->ReadUint32();
+	}
+
+	void ReadStreamHeader()
+	{
+		AVIStreamHeader streamHeader;
+		reader->PushChunk("strh");
+		if (reader->GetChunkSize() < 56)
+			throw std::runtime_error("Unsupported AVI stream header size");
+		streamHeader.StreamType = reader->ReadTag();
+		streamHeader.Codec = reader->ReadTag();
+		streamHeader.Flags = reader->ReadUint32();
+		streamHeader.Priority = reader->ReadUint16();
+		streamHeader.Language = reader->ReadUint16();
+		streamHeader.InitialFrames = reader->ReadUint32(); // How far ahead the audio compared to the video
+		streamHeader.Scale = reader->ReadUint32();
+		streamHeader.Rate = reader->ReadUint32(); // Rate / Scale = samples per second
+		streamHeader.Start = reader->ReadUint32(); // Start time
+		streamHeader.Length = reader->ReadUint32(); // Length of video
+		streamHeader.SuggestedBufferSize = reader->ReadUint32();
+		streamHeader.Quality = reader->ReadUint32();
+		streamHeader.SampleSize = reader->ReadUint32(); // Block align for audio, zero if varying
+		streamHeader.Frame.Left = reader->ReadInt16();
+		streamHeader.Frame.Top = reader->ReadInt16();
+		streamHeader.Frame.Right = reader->ReadInt16();
+		streamHeader.Frame.Bottom = reader->ReadInt16();
+		reader->PopChunk();
+		reader->PushChunk("strf");
+		if (streamHeader.StreamType == "vids")
+		{
+			ReadVideoStreamHeader(streamHeader);
+		}
+		else if (streamHeader.StreamType == "auds")
+		{
+			ReadAudioStreamHeader(streamHeader);
+		}
+		reader->PopChunk();
+		streamHeaders.push_back(streamHeader);
+	}
+
+	void ReadVideoStreamHeader(AVIStreamHeader& streamHeader)
+	{
+		if (reader->GetChunkSize() < 40)
+			throw std::runtime_error("Unsupported AVI video stream header size");
+		int size = reader->ReadUint32(); // cbSize in BITMAPINFOHEADER
+		if (size < 40)
+			throw std::runtime_error("Unsupported AVI video stream header size");
+		streamHeader.Video.Width = reader->ReadUint32();
+		streamHeader.Video.Height = reader->ReadUint32();
+		streamHeader.Video.Planes = reader->ReadUint16();
+		streamHeader.Video.BitCount = reader->ReadUint16();
+		streamHeader.Video.Compression = reader->ReadUint32();
+		streamHeader.Video.ImageSize = reader->ReadUint32();
+		streamHeader.Video.XPixelsPerMeter = reader->ReadUint32();
+		streamHeader.Video.YPixelsPerMeter = reader->ReadUint32();
+		streamHeader.Video.ColorsUsed = reader->ReadUint32();
+		streamHeader.Video.ColorsImportant = reader->ReadUint32();
+	}
+
+	void ReadAudioStreamHeader(AVIStreamHeader& streamHeader)
+	{
+		if (reader->GetChunkSize() < 18)
+			throw std::runtime_error("Unsupported AVI audio stream header size");
+		streamHeader.Audio.FormatTag = reader->ReadUint16();
+		streamHeader.Audio.Channels = reader->ReadUint16();
+		streamHeader.Audio.SamplesPerSec = reader->ReadUint32();
+		streamHeader.Audio.AvgSamplesPerSec = reader->ReadUint32();
+		streamHeader.Audio.BlockAlign = reader->ReadUint16();
+		streamHeader.Audio.BitsPerSample = reader->ReadUint16();
+		streamHeader.Audio.SizeExtra = reader->ReadUint16();
+		if (streamHeader.Audio.FormatTag == 2 /*WAVE_FORMAT_ADPCM*/ && streamHeader.Audio.SizeExtra == 32)
+		{
+			// This nasty thing:
+			// https://wiki.multimedia.cx/index.php/Microsoft_ADPCM
+		}
+		else if (streamHeader.Audio.FormatTag == 0xfffe /*WAVE_FORMAT_EXTENSIBLE*/ && streamHeader.Audio.SizeExtra >= 22)
+		{
+			streamHeader.Audio.SamplesPerBlock = reader->ReadUint16();
+			streamHeader.Audio.ChannelMask = reader->ReadUint32();
+			reader->Read(streamHeader.Audio.SubFormat, 16);
 		}
 	}
 
