c1e9ba8f0d21265406668d6d0c6aa92e833adb99
Magnus Norddahl <dpjudas@users.noreply.github.com>
2025-09-10 22:43:51 +0200

    Add win32 icon and manifest

---
+++ Win32Resources/version.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_VERSION_H
+#define HEADER_VERSION_H
+
+#define RC_FILEVERSION 1,0,0,0
+#define RC_PRODUCTVERSION 1,0,0,0
+#define RC_PRODUCT_NAME "SurrealEngine"
+#define RC_FILENAME "SurrealEngine.exe"
+#define RC_FILE_VERSION "1.0.0"
+#define RC_VERSION_DESCRIPTION "1.0.0"
+#define RC_FILE_DESCRIPTION "Surreal Engine"
+#define RC_COPYRIGHT "Copyright Surreal Engine Development Team"
+#define RC_COMPANY_NAME "Surreal Engine Development Team"
+
+#endif
