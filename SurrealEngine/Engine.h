43a638d2ee22b0c5ba8eefad833038551649665d
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2025-10-09 23:33:22 +0300

    Engine: Add more logging: - Added logging for individual steps (booting/shutdown) - SHA1Sums of some .u files are also logged

--- SurrealEngine/Engine.h
+++ SurrealEngine/Engine.h
@@ -209,6 +209,8 @@ public:
 private:
 	std::map<std::string, std::string> CreateTravelInfo(bool transferItems);
 
+	void LogGamePackageSHA1Sums() const;
+
 	bool m_EditorMode = false; // Set this to true to allow rendering of invisible polys.
 	bool m_GamePaused = false;
 
