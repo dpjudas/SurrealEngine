f61999a0d52caae0a665155d6ee53228b3e39e27
PentaChess413 <mlma38692@gmail.com>
2026-02-20 12:23:50 -0300

    Implemented InitRootWindow stub.

--- SurrealEngine/Native/NPlayerPawnExt.h
+++ SurrealEngine/Native/NPlayerPawnExt.h
@@ -7,6 +7,7 @@ class NPlayerPawnExt
 public:
 	static void RegisterFunctions();
 
+	static void ConstructRootWindow(UObject* Self);
 	static void InitRootWindow(UObject* Self);
 	static void PostRenderWindows(UObject* Self, UObject* Canvas);
 	static void PreRenderWindows(UObject* Self, UObject* Canvas);
