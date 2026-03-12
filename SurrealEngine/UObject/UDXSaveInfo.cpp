6ca3f163b8b41f6fe9d986d6241926ed9493e6cf
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-03-12 19:06:25 +0300

    Add UDXSaveInfo

---
+++ SurrealEngine/UObject/UDXSaveInfo.cpp
@@ -0,0 +1,17 @@
+#include "UDXSaveInfo.h"
+
+#include <ctime>
+
+void UDXSaveInfo::UpdateTimeStamp()
+{
+    // Update the time fields
+    std::time_t now = std::time(nullptr);
+    std::tm* timedesc = std::localtime(&now);
+
+    Year() = timedesc->tm_year;
+    Month() = timedesc->tm_mon;
+    Day() = timedesc->tm_mday;
+    Hour() = timedesc->tm_hour;
+    Minute() = timedesc->tm_min;
+    Second() = timedesc->tm_sec;
+}
