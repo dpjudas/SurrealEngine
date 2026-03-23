7d62a3b024ca0b23426f298266a192e0312f76ea
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-03-23 02:28:12 +0100

    Add noexcept

--- SurrealEngine/Compiler/Frontend/CompilerException.h
+++ SurrealEngine/Compiler/Frontend/CompilerException.h
@@ -10,7 +10,7 @@ public:
 	CompilerException(const std::string &message) : _message(message) { }
 	const std::string &message() const { return _message; }
 
-	const char *what() const override { return _message.c_str(); }
+	const char *what() const noexcept override { return _message.c_str(); }
 
 private:
 	std::string _message;
