47d93ee4853efd8610f32d9c6310e35fe240aaac
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-03-24 03:12:51 +0300

    Random.cpp/.h: Add

---
+++ SurrealEngine/Utils/Random.h
@@ -0,0 +1,4 @@
+int RandInt(int Min, int Max); // A random integer value between [Min, Max]
+int RandInt(int Max); // A random integer value between [0, Max]
+float FRand(); // A random float value between [0, 1]
+float FRandRange(float Min, float Max);
