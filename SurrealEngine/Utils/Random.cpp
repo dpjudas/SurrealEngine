5776f1ddaa74e4b3c448e1fae747b3995143915d
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-03-24 03:12:51 +0300

    Random.cpp/.h: Add

---
+++ SurrealEngine/Utils/Random.cpp
@@ -0,0 +1,29 @@
+#include "Random.h"
+
+#include <random>
+
+static std::default_random_engine s_Generator;
+
+int RandInt(int Min, int Max)
+{
+    std::uniform_int_distribution<int> distribution(Min, Max);
+
+    return distribution(s_Generator);
+}
+
+int RandInt(int Max)
+{
+    return RandInt(0, Max);
+}
+
+float FRand()
+{
+    return FRandRange(0.0f, 1.0f);
+}
+
+float FRandRange(float Min, float Max)
+{
+    std::uniform_real_distribution<float> distribution(Min, Max);
+
+    return distribution(s_Generator);
+}
