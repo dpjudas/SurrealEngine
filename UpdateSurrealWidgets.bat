61d3c1f3554ef954bfeb31445e0567e96b49d242
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-04-04 11:57:26 +0200

    Update subtree script

---
+++ UpdateSurrealWidgets.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem git subtree add --prefix SurrealWidgets https://github.com/dpjudas/SurrealWidgets.git master --squash
+git subtree pull --prefix SurrealWidgets https://github.com/dpjudas/SurrealWidgets.git master --squash
--- UpdateZWidget.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-git subtree pull --prefix Thirdparty/ZWidget https://github.com/dpjudas/ZWidget.git master --squash
