e5d08dad2c211ec86fee213d78853e7a0597a0d9
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-04-12 04:44:19 +0300

    U227Emitter: Add stubs for missing functions in UXEmitter, add UXRainRestrictionVolume

--- SurrealEngine/UObject/U227Emitter.cpp
+++ SurrealEngine/UObject/U227Emitter.cpp
@@ -6,3 +6,23 @@ void UXParticleEmitter::SetParticlesProps(std::optional<float> Speed, std::optio
 {
     LogUnimplemented("XParticleEmitter.SetParticlesProps() [U227]");
 }
+
+void UXEmitter::EmTrigger()
+{
+    LogUnimplemented("XEmitter.EmTrigger() [U227]");
+}
+
+void UXEmitter::Kill()
+{
+    LogUnimplemented("XEmitter.Kill() [U227]");
+}
+
+void UXEmitter::SetMaxParticles(int MaxParts)
+{
+    LogUnimplemented("XEmitter.SetMaxParticles() [U227]");
+}
+
+void UXEmitter::SpawnParticles(int Count)
+{
+    LogUnimplemented("XEmitter.SpawnParticles() [U227]");
+}
