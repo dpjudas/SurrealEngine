5b938d4f9f3e63cf41b275283b542e01c1424261
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-05-02 22:31:56 +0300

    Add native functions of GameInfo introduced in U227

--- SurrealEngine/Native/NGameInfo.h
+++ SurrealEngine/Native/NGameInfo.h
@@ -9,4 +9,8 @@ public:
 
 	static void GetNetworkNumber(UObject* Self, std::string& ReturnValue);
 	static void ParseKillMessage(const std::string& KillerName, const std::string& VictimName, const std::string& WeaponName, const std::string& DeathMessage, std::string& ReturnValue);
+	static void GetNetworkNumber_U227(UObject* Self, std::string& ReturnValue);
+	static void LoadTravelInventory_U227(UObject* Other, BitfieldBool& ReturnValue);
+	static void MakeColorCode_U227(const Color& color, std::string& ReturnValue);
+	static void StripColorCodes_U227(std::string& S, std::string& ReturnValue);
 };
