82a1265378184dc87d5037cff6c1c5c12c274fc4
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-05-02 22:31:56 +0300

    Add native functions of GameInfo introduced in U227

--- SurrealEngine/Native/NGameInfo.cpp
+++ SurrealEngine/Native/NGameInfo.cpp
@@ -3,11 +3,22 @@
 #include "NGameInfo.h"
 #include "VM/NativeFunc.h"
 #include "Engine.h"
+#include "Package/PackageManager.h"
+
+#include <sstream>
 
 void NGameInfo::RegisterFunctions()
 {
 	RegisterVMNativeFunc_1("GameInfo", "GetNetworkNumber", &NGameInfo::GetNetworkNumber, 0);
 	RegisterVMNativeFunc_5("GameInfo", "ParseKillMessage", &NGameInfo::ParseKillMessage, 0);
+	if (engine->LaunchInfo.IsUnreal1_227())
+	{
+		RegisterVMNativeFunc_2("GameInfo", "GetNetworkNumber", &NGameInfo::GetNetworkNumber_U227, 0);
+		RegisterVMNativeFunc_2("GameInfo", "LoadTravelInventory", &NGameInfo::LoadTravelInventory_U227, 920);
+		RegisterVMNativeFunc_2("GameInfo", "MakeColorCode", &NGameInfo::MakeColorCode_U227, 0);
+		RegisterVMNativeFunc_2("GameInfo", "StripColorCodes", &NGameInfo::StripColorCodes_U227, 0);
+	}
+
 }
 
 void NGameInfo::GetNetworkNumber(UObject* Self, std::string& ReturnValue)
@@ -48,4 +59,41 @@ void NGameInfo::ParseKillMessage(const std::string& KillerName, const std::strin
 	}
 
 	ReturnValue = result;
+}
+
+void NGameInfo::GetNetworkNumber_U227(UObject* Self, std::string& ReturnValue)
+{
+	// auto GISelf = UObject::Cast<UGameInfo>(Self);
+	LogUnimplemented("GameInfo.GetNetworkNumber() [U227]");
+	ReturnValue = "";
+}
+
+void NGameInfo::LoadTravelInventory_U227(UObject* Other, BitfieldBool& ReturnValue)
+{
+	// auto PPOther = UObject::Cast<UPlayerPawn>(Other);
+	LogUnimplemented("GameInfo.LoadTravelInventory() [U227]");
+	ReturnValue = false;
+}
+
+void NGameInfo::MakeColorCode_U227(const Color& color, std::string& ReturnValue)
+{
+	std::stringstream ss;
+	ss << "#";
+	ss << std::hex << std::setfill('0') << std::setw(2);
+	ss << color.R << color.G << color.B << color.A;
+
+	ReturnValue = ss.str();
+}
+
+void NGameInfo::StripColorCodes_U227(std::string& S, std::string& ReturnValue)
+{
+	auto pos = S.find('#');
+
+	while (pos != std::string::npos)
+	{
+		S.erase(pos, 9); // #RRGGBBAA
+		pos = S.find('#');
+	}
+
+	ReturnValue = S;
 }
