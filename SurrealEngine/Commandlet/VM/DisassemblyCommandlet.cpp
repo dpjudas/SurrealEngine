b0b494da9b93c713ab0f8a875b244bfaf6d00c89
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-05-31 02:56:43 +0200

    Improve disassembly print

--- SurrealEngine/Commandlet/VM/DisassemblyCommandlet.cpp
+++ SurrealEngine/Commandlet/VM/DisassemblyCommandlet.cpp
@@ -24,7 +24,17 @@ void DisassemblyCommandlet::OnCommand(DebuggerApp* console, const std::string& a
 		for (Expression* expr : frame->Func->Code->Statements)
 		{
 			if (expr == Frame::StepExpression)
-				PrintExpression::Print(console, "Statement[" + std::to_string(index) + "]", expr);
+			{
+				if (args == "full")
+				{
+					PrintExpression::Print(console, "Statement[" + std::to_string(index) + "]", expr);
+				}
+				else
+				{
+					PrintPrettyExpression::Print(console, expr);
+					console->WriteOutput(console->NewLine());
+				}
+			}
 			index++;
 		}
 	}
@@ -644,6 +654,665 @@ void PrintExpression::Expr(ConstructExpression* expr)
 }
 
 std::string PrintExpression::GetFullFuncName(UFunction* func)
+{
+	std::string name;
+	for (UStruct* s = func; s != nullptr; s = s->StructParent)
+	{
+		if (name.empty())
+			name = s->Name.ToString();
+		else
+			name = s->Name.ToString() + "." + name;
+	}
+	return name;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void PrintPrettyExpression::Print(DebuggerApp* console, Expression* expr)
+{
+	PrintPrettyExpression builder;
+	builder.console = console;
+
+	if (expr)
+	{
+		expr->Visit(&builder);
+	}
+	else
+	{
+		console->WriteOutput("Null");
+	}
+}
+
+void PrintPrettyExpression::Expr(LocalVariableExpression* expr)
+{
+	if (expr->Variable)
+		console->WriteOutput(expr->Variable->Name.ToString());
+	else
+		console->WriteOutput("(null)");
+}
+
+void PrintPrettyExpression::Expr(InstanceVariableExpression* expr)
+{
+	if (expr->Variable)
+		console->WriteOutput("" + expr->Variable->Name.ToString());
+	else
+		console->WriteOutput("(null)");
+}
+
+void PrintPrettyExpression::Expr(DefaultVariableExpression* expr)
+{
+	if (expr->Variable)
+		console->WriteOutput("default."+ expr->Variable->Name.ToString());
+	else
+		console->WriteOutput("default.(null)");
+}
+
+void PrintPrettyExpression::Expr(ReturnExpression* expr)
+{
+	console->WriteOutput("return");
+	if (expr->Value)
+	{
+		console->WriteOutput(" ");
+		Print(console, expr->Value);
+	}
+}
+
+void PrintPrettyExpression::Expr(SwitchExpression* expr)
+{
+	console->WriteOutput("switch (");
+	Print(console, expr->Condition);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(JumpExpression* expr)
+{
+	console->WriteOutput("jump");
+}
+
+void PrintPrettyExpression::Expr(JumpIfNotExpression* expr)
+{
+	console->WriteOutput("if (");
+	Print(console, expr->Condition);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StopExpression* expr)
+{
+	console->WriteOutput("stop");
+}
+
+void PrintPrettyExpression::Expr(AssertExpression* expr)
+{
+	console->WriteOutput("assert(");
+	Print(console, expr->Condition);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(CaseExpression* expr)
+{
+	console->WriteOutput("case ");
+	Print(console, expr->Value);
+	console->WriteOutput(":");
+}
+
+void PrintPrettyExpression::Expr(NothingExpression* expr)
+{
+	console->WriteOutput("nothing");
+}
+
+void PrintPrettyExpression::Expr(LabelTableExpression* expr)
+{
+	console->WriteOutput("label table");
+}
+
+void PrintPrettyExpression::Expr(GotoLabelExpression* expr)
+{
+	console->WriteOutput("goto ");
+	Print(console, expr->Value);
+}
+
+void PrintPrettyExpression::Expr(EatStringExpression* expr)
+{
+	console->WriteOutput("eat string(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(LetExpression* expr)
+{
+	Print(console, expr->LeftSide);
+	console->WriteOutput(" = ");
+	Print(console, expr->RightSide);
+}
+
+void PrintPrettyExpression::Expr(DynArrayElementExpression* expr)
+{
+	Print(console, expr->Array);
+	console->WriteOutput("[");
+	Print(console, expr->Index);
+	console->WriteOutput("]");
+}
+
+void PrintPrettyExpression::Expr(NewExpression* expr)
+{
+	console->WriteOutput("new(parent: ");
+	Print(console, expr->ParentExpr);
+	console->WriteOutput(", name: ");
+	Print(console, expr->NameExpr);
+	console->WriteOutput(", flags: ");
+	Print(console, expr->FlagsExpr);
+	console->WriteOutput(", class: ");
+	Print(console, expr->ClassExpr);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ClassContextExpression* expr)
+{
+	console->WriteOutput("class<");
+	Print(console, expr->ObjectExpr);
+	console->WriteOutput(".");
+	Print(console, expr->ContextExpr);
+	console->WriteOutput(">");
+}
+
+void PrintPrettyExpression::Expr(MetaCastExpression* expr)
+{
+	console->WriteOutput("meta cast(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(LetBoolExpression* expr)
+{
+	Print(console, expr->LeftSide);
+	console->WriteOutput(" = ");
+	Print(console, expr->RightSide);
+}
+
+void PrintPrettyExpression::Expr(Unknown0x15Expression* expr)
+{
+	console->WriteOutput("unknown(0x15)");
+}
+
+void PrintPrettyExpression::Expr(SelfExpression* expr)
+{
+	console->WriteOutput("this");
+}
+
+void PrintPrettyExpression::Expr(SkipExpression* expr)
+{
+	console->WriteOutput("skip(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ContextExpression* expr)
+{
+	Print(console, expr->ObjectExpr);
+	console->WriteOutput(".");
+	Print(console, expr->ContextExpr);
+}
+
+void PrintPrettyExpression::Expr(ArrayElementExpression* expr)
+{
+	Print(console, expr->Array);
+	console->WriteOutput("[");
+	Print(console, expr->Index);
+	console->WriteOutput("]");
+}
+
+void PrintPrettyExpression::Expr(IntConstExpression* expr)
+{
+	console->WriteOutput(std::to_string(expr->Value));
+}
+
+void PrintPrettyExpression::Expr(FloatConstExpression* expr)
+{
+	console->WriteOutput(std::to_string(expr->Value));
+}
+
+void PrintPrettyExpression::Expr(StringConstExpression* expr)
+{
+	console->WriteOutput("\"" + expr->Value + "\"");
+}
+
+void PrintPrettyExpression::Expr(ObjectConstExpression* expr)
+{
+	if (expr->Object)
+		console->WriteOutput("{ name=\"" + expr->Object->Name.ToString() + "\", class=" + UObject::GetUClassName(expr->Object).ToString() + " }");
+	else
+		console->WriteOutput("null");
+}
+
+void PrintPrettyExpression::Expr(NameConstExpression* expr)
+{
+	console->WriteOutput(expr->Value.ToString());
+}
+
+void PrintPrettyExpression::Expr(RotationConstExpression* expr)
+{
+	console->WriteOutput("{ " + std::to_string(expr->Pitch) + ", " + std::to_string(expr->Yaw) + ", " + std::to_string(expr->Roll) + " }");
+}
+
+void PrintPrettyExpression::Expr(VectorConstExpression* expr)
+{
+	console->WriteOutput("{ " + std::to_string(expr->X) + ", " + std::to_string(expr->Y) + ", " + std::to_string(expr->Z) + " }");
+}
+
+void PrintPrettyExpression::Expr(ByteConstExpression* expr)
+{
+	console->WriteOutput(std::to_string(expr->Value));
+}
+
+void PrintPrettyExpression::Expr(IntZeroExpression* expr)
+{
+	console->WriteOutput("0");
+}
+
+void PrintPrettyExpression::Expr(IntOneExpression* expr)
+{
+	console->WriteOutput("1");
+}
+
+void PrintPrettyExpression::Expr(TrueExpression* expr)
+{
+	console->WriteOutput("true");
+}
+
+void PrintPrettyExpression::Expr(FalseExpression* expr)
+{
+	console->WriteOutput("false");
+}
+
+void PrintPrettyExpression::Expr(NativeParmExpression* expr)
+{
+	if (expr->Object)
+		console->WriteOutput(expr->Object->Name.ToString());
+	else
+		console->WriteOutput("(null)");
+}
+
+void PrintPrettyExpression::Expr(NoObjectExpression* expr)
+{
+	console->WriteOutput("null");
+}
+
+void PrintPrettyExpression::Expr(Unknown0x2bExpression* expr)
+{
+	console->WriteOutput("unknown(0x2b, ");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IntConstByteExpression* expr)
+{
+	console->WriteOutput(std::to_string(expr->Value));
+}
+
+void PrintPrettyExpression::Expr(BoolVariableExpression* expr)
+{
+	Print(console, expr->Variable);
+}
+
+void PrintPrettyExpression::Expr(DynamicCastExpression* expr)
+{
+	console->WriteOutput("dynamic cast(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IteratorExpression* expr)
+{
+	console->WriteOutput("iterator(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IteratorPopExpression* expr)
+{
+	console->WriteOutput("iterator pop");
+}
+
+void PrintPrettyExpression::Expr(IteratorNextExpression* expr)
+{
+	console->WriteOutput("iterator next");
+}
+
+void PrintPrettyExpression::Expr(StructCmpEqExpression* expr)
+{
+	Print(console, expr->Value1);
+	console->WriteOutput(" == ");
+	Print(console, expr->Value2);
+}
+
+void PrintPrettyExpression::Expr(StructCmpNeExpression* expr)
+{
+	Print(console, expr->Value1);
+	console->WriteOutput(" != ");
+	Print(console, expr->Value2);
+}
+
+void PrintPrettyExpression::Expr(UnicodeStringConstExpression* expr)
+{
+	console->WriteOutput("unicode string");
+}
+
+void PrintPrettyExpression::Expr(StructMemberExpression* expr)
+{
+	Print(console, expr->Value);
+	console->WriteOutput(".");
+	console->WriteOutput(expr->Field->Name.ToString());
+}
+
+void PrintPrettyExpression::Expr(RotatorToVectorExpression* expr)
+{
+	console->WriteOutput("rotatorToVector(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ByteToIntExpression* expr)
+{
+	console->WriteOutput("byteToInt(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ByteToBoolExpression* expr)
+{
+	console->WriteOutput("byteToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ByteToFloatExpression* expr)
+{
+	console->WriteOutput("byteToFloat(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IntToByteExpression* expr)
+{
+	console->WriteOutput("intToByte(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IntToBoolExpression* expr)
+{
+	console->WriteOutput("intToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IntToFloatExpression* expr)
+{
+	console->WriteOutput("intToFloat(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(BoolToByteExpression* expr)
+{
+	console->WriteOutput("boolToByte(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(BoolToIntExpression* expr)
+{
+	console->WriteOutput("boolToInt(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(BoolToFloatExpression* expr)
+{
+	console->WriteOutput("boolToFloat(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FloatToByteExpression* expr)
+{
+	console->WriteOutput("floatToByte(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FloatToIntExpression* expr)
+{
+	console->WriteOutput("floatToInt(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FloatToBoolExpression* expr)
+{
+	console->WriteOutput("floatToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(Unknown0x46Expression* expr)
+{
+	console->WriteOutput("unknown(0x46, ");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ObjectToBoolExpression* expr)
+{
+	console->WriteOutput("objectToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(NameToBoolExpression* expr)
+{
+	console->WriteOutput("nameToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToByteExpression* expr)
+{
+	console->WriteOutput("stringToByte(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToIntExpression* expr)
+{
+	console->WriteOutput("stringToInt(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToBoolExpression* expr)
+{
+	console->WriteOutput("stringToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToFloatExpression* expr)
+{
+	console->WriteOutput("stringToFloat(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToVectorExpression* expr)
+{
+	console->WriteOutput("stringToVector(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(StringToRotatorExpression* expr)
+{
+	console->WriteOutput("stringToRotator(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(VectorToBoolExpression* expr)
+{
+	console->WriteOutput("vectorToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(VectorToRotatorExpression* expr)
+{
+	console->WriteOutput("vectorToRotator(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(RotatorToBoolExpression* expr)
+{
+	console->WriteOutput("rotatorToBool(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ByteToStringExpression* expr)
+{
+	console->WriteOutput("byteToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(IntToStringExpression* expr)
+{
+	console->WriteOutput("intToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(BoolToStringExpression* expr)
+{
+	console->WriteOutput("boolToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FloatToStringExpression* expr)
+{
+	console->WriteOutput("floatToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(ObjectToStringExpression* expr)
+{
+	console->WriteOutput("objectToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(NameToStringExpression* expr)
+{
+	console->WriteOutput("nameToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(VectorToStringExpression* expr)
+{
+	console->WriteOutput("vectorToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(RotatorToStringExpression* expr)
+{
+	console->WriteOutput("rotatorToString(");
+	Print(console, expr->Value);
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(VirtualFunctionExpression* expr)
+{
+	console->WriteOutput(expr->Name.ToString() + "(");
+	int index = 0;
+	for (auto arg : expr->Args)
+	{
+		if (index != 0)
+			console->WriteOutput(", ");
+		Print(console, arg);
+		index++;
+	}
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FinalFunctionExpression* expr)
+{
+	console->WriteOutput(GetFullFuncName(expr->Func) + "(");
+	int index = 0;
+	for (auto arg : expr->Args)
+	{
+		if (index != 0)
+			console->WriteOutput(", ");
+		Print(console, arg);
+		index++;
+	}
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(GlobalFunctionExpression* expr)
+{
+	console->WriteOutput(expr->Name.ToString() + "(");
+	int index = 0;
+	for (auto arg : expr->Args)
+	{
+		if (index != 0)
+			console->WriteOutput(", ");
+		Print(console, arg);
+		index++;
+	}
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(NativeFunctionExpression* expr)
+{
+	console->WriteOutput(GetFullFuncName(NativeFunctions::FuncByIndex[expr->nativeindex]) + "(");
+	int index = 0;
+	for (auto arg : expr->Args)
+	{
+		if (index != 0)
+			console->WriteOutput(", ");
+		Print(console, arg);
+		index++;
+	}
+	console->WriteOutput(")");
+}
+
+void PrintPrettyExpression::Expr(FunctionArgumentsExpression* expr)
+{
+	console->WriteOutput("function arguments");
+}
+
+void PrintPrettyExpression::Expr(ConstructExpression* expr)
+{
+	console->WriteOutput("construct " + expr->Struct->Name.ToString() + "(");
+	int index = 0;
+	for (auto arg : expr->Args)
+	{
+		if (index != 0)
+			console->WriteOutput(", ");
+		if (arg.Name)
+			console->WriteOutput(arg.Name->Name.ToString() + ": ");
+		Print(console, arg.Value);
+		index++;
+	}
+	console->WriteOutput(")");
+}
+
+std::string PrintPrettyExpression::GetFullFuncName(UFunction* func)
 {
 	std::string name;
 	for (UStruct* s = func; s != nullptr; s = s->StructParent)
