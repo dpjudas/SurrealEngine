cf50a2548b6dae2143a67f70d883783b97d096c0
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-07-12 01:33:57 +0300

    NLevelInfo: Add stubs for AllocateObj()/FreeObject(), fix registration of GetLocZone()

--- SurrealEngine/Native/NLevelInfo.h
+++ SurrealEngine/Native/NLevelInfo.h
@@ -9,6 +9,8 @@ class NLevelInfo
 public:
 	static void RegisterFunctions();
 
+	static void AllocateObj_U227(UObject* Self, UObject* ObjClass, UObject*& ReturnValue);
+	static void FreeObject_U227(UObject* Self, UObject* Obj);
 	static void GetAddressURL(UObject* Self, std::string& ReturnValue);
 	static void GetLocalURL(UObject* Self, std::string& ReturnValue);
 	static void GetLocZone_U227(UObject* Self, const vec3& Pos, PointRegion& ReturnValue);
