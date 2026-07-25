0747cbc2a2953da6aad778455ed6c3767dddb176
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-07-25 03:28:53 +0200

    Remove redundant check

--- SurrealEngine/UObject/UObject.cpp
+++ SurrealEngine/UObject/UObject.cpp
@@ -359,8 +359,6 @@ bool UObject::IsA(const NameString& className) const
 	UStruct* cls = Class;
 	while (cls)
 	{
-		if (!Class)
-			return false;
 		if (cls->Name == className)
 			return true;
 		cls = cls->BaseStruct;
