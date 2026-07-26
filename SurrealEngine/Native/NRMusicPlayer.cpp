b397ad5cd13d156c86f25d17faa8b3afece9a492
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-07-26 13:23:08 +0300

    Add missing Self parameters to NRMusicPlayer functions

--- SurrealEngine/Native/NRMusicPlayer.cpp
+++ SurrealEngine/Native/NRMusicPlayer.cpp
@@ -31,29 +31,29 @@ void NRMusicPlayer::RegisterFunctions()
     RegisterVMNativeFunc_1("RMusic_Player", "RMusic_GetCurrentTime", &NRMusicPlayer::RMusic_GetCurrentTime, 0);
 }
 
-void NRMusicPlayer::RMusic_Startup(BitfieldBool& ReturnValue)
+void NRMusicPlayer::RMusic_Startup(UObject* Self, BitfieldBool& ReturnValue)
 {
     // Since we'll use the built-in music support, we don't have to initialize anything here.
     ReturnValue = true;
 }
 
-void NRMusicPlayer::RMusic_LoadPlugin(std::string& Plugin, BitfieldBool& ReturnValue)
+void NRMusicPlayer::RMusic_LoadPlugin(UObject* Self, std::string& Plugin, BitfieldBool& ReturnValue)
 {
     // no-op
 }
 
-void NRMusicPlayer::RMusic_UnloadPlugin(std::string& Plugin)
+void NRMusicPlayer::RMusic_UnloadPlugin(UObject* Self, std::string& Plugin)
 {
     // no-op
 }
 
-void NRMusicPlayer::RMusic_SetDSPParam(std::string& Plugin, int index, float value)
+void NRMusicPlayer::RMusic_SetDSPParam(UObject* Self, std::string& Plugin, int index, float value)
 {
     // maybe a no-op?
     LogUnimplemented("RMusic_Player.RMusic_SetDSPParam()");
 }
 
-void NRMusicPlayer::RMusic_Update()
+void NRMusicPlayer::RMusic_Update(UObject* Self)
 {
     // Since we don't use fmod, this is a no-op
 }
@@ -64,7 +64,7 @@ void NRMusicPlayer::RMusic_Play(UObject* Self, std::string& File, bool Loop, Bit
     ReturnValue = MusicPlayer->RMusic_Play(File, Loop);
 }
 
-void NRMusicPlayer::RMusic_Pause(bool bPause)
+void NRMusicPlayer::RMusic_Pause(UObject* Self, bool bPause)
 {
     LogUnimplemented("RMusic_Player.RMusic_Pause()");
 }
@@ -75,7 +75,7 @@ void NRMusicPlayer::RMusic_IsPlaying(UObject* Self, BitfieldBool& ReturnValue)
     ReturnValue = MusicPlayer->RMusic_IsPlaying();
 }
 
-void NRMusicPlayer::RMusic_SetCfgVolume()
+void NRMusicPlayer::RMusic_SetCfgVolume(UObject* Self)
 {
     // Probably not a no-op
     LogUnimplemented("RMusic_Player.RMusic_SetCfgVolume()");
@@ -87,12 +87,12 @@ void NRMusicPlayer::RMusic_SetVolume(UObject* Self, int NewVolume)
     MusicPlayer->RMusic_SetNewVolume(NewVolume);
 }
 
-void NRMusicPlayer::RMusic_IncVolume()
+void NRMusicPlayer::RMusic_IncVolume(UObject* Self)
 {
     LogUnimplemented("RMusic_Player.RMusic_IncVolume()");
 }
 
-void NRMusicPlayer::RMusic_DecVolume()
+void NRMusicPlayer::RMusic_DecVolume(UObject* Self)
 {
     LogUnimplemented("RMusic_Player.RMusic_DecVolume()");
 }
@@ -107,18 +107,18 @@ void NRMusicPlayer::RMusic_Stop(UObject* Self)
     UObject::Cast<URMusic_Player>(Self)->RMusic_Stop();
 }
 
-void NRMusicPlayer::RMusic_Close()
+void NRMusicPlayer::RMusic_Close(UObject* Self)
 {
     // no-op
 }
 
-void NRMusicPlayer::RMusic_GetTotalTime(int& ReturnValue)
+void NRMusicPlayer::RMusic_GetTotalTime(UObject* Self, int& ReturnValue)
 {
     LogUnimplemented("RMusic_Player.RMusic_GetTotalTime()");
     ReturnValue = -1;
 }
 
-void NRMusicPlayer::RMusic_GetCurrentTime(int& ReturnValue)
+void NRMusicPlayer::RMusic_GetCurrentTime(UObject* Self, int& ReturnValue)
 {
     LogUnimplemented("RMusic_Player.RMusic_GetCurrentTime()");
     ReturnValue = -1;
