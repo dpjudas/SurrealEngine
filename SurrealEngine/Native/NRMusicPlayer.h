5110772852de842640e2c417506be3ca892833ee
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-07-26 13:23:08 +0300

    Add missing Self parameters to NRMusicPlayer functions

--- SurrealEngine/Native/NRMusicPlayer.h
+++ SurrealEngine/Native/NRMusicPlayer.h
@@ -12,21 +12,21 @@ class NRMusicPlayer
 public:
     static void RegisterFunctions();
 
-    static void RMusic_Startup(BitfieldBool& ReturnValue);
-    static void RMusic_LoadPlugin(std::string& Plugin, BitfieldBool& ReturnValue);
-    static void RMusic_UnloadPlugin(std::string& Plugin);
-    static void RMusic_SetDSPParam(std::string& Plugin, int index, float value);
-    static void RMusic_Update();
+    static void RMusic_Startup(UObject* Self, BitfieldBool& ReturnValue);
+    static void RMusic_LoadPlugin(UObject* Self, std::string& Plugin, BitfieldBool& ReturnValue);
+    static void RMusic_UnloadPlugin(UObject* Self, std::string& Plugin);
+    static void RMusic_SetDSPParam(UObject* Self, std::string& Plugin, int index, float value);
+    static void RMusic_Update(UObject* Self);
     static void RMusic_Play(UObject* Self, std::string& File, bool Loop, BitfieldBool& ReturnValue);
-    static void RMusic_Pause(bool bPause);
+    static void RMusic_Pause(UObject* Self, bool bPause);
     static void RMusic_IsPlaying(UObject* Self, BitfieldBool& ReturnValue);
-    static void RMusic_SetCfgVolume();
+    static void RMusic_SetCfgVolume(UObject* Self);
     static void RMusic_SetVolume(UObject* Self, int NewVolume);
-    static void RMusic_IncVolume();
-    static void RMusic_DecVolume();
+    static void RMusic_IncVolume(UObject* Self);
+    static void RMusic_DecVolume(UObject* Self);
     static void RMusic_GetVolume(UObject* Self, int& ReturnValue);
     static void RMusic_Stop(UObject* Self);
-    static void RMusic_Close();
-    static void RMusic_GetTotalTime(int& ReturnValue);
-    static void RMusic_GetCurrentTime(int& ReturnValue);
+    static void RMusic_Close(UObject* Self);
+    static void RMusic_GetTotalTime(UObject* Self, int& ReturnValue);
+    static void RMusic_GetCurrentTime(UObject* Self, int& ReturnValue);
 };
