325430c9e68532192f40cf7294679129a9a0531d
Magnus Norddahl <dpjudas@users.noreply.github.com>
2026-07-30 17:51:46 +0200

    Implement drawing and mouse input for UCheckboxWindow

--- SurrealEngine/Packages/Extension/Windows/Text/UCheckboxWindow.cpp
+++ SurrealEngine/Packages/Extension/Windows/Text/UCheckboxWindow.cpp
@@ -1,7 +1,10 @@
 
 #include "Precomp.h"
 #include "UCheckboxWindow.h"
+#include "Packages/Extension/Windows/UGC.h"
+#include "Packages/Engine/Resources/UFont.h"
 #include "Packages/Engine/Resources/Textures/UTexture.h"
+#include "Engine.h"
 
 void UCheckboxWindow::SetCheckboxColor(const Color& NewColor)
 {
@@ -33,4 +36,108 @@ void UCheckboxWindow::SetCheckboxTextures(std::optional<UObject*> newToggleOff, 
 void UCheckboxWindow::ShowCheckboxOnRightSide(std::optional<bool> bRight)
 {
 	bRightSide() = !bRight || *bRight;
+}
+
+void UCheckboxWindow::InitWindow()
+{
+	UToggleWindow::InitWindow();
+}
+
+void UCheckboxWindow::DrawWindow(UGC* gc)
+{
+	UWindow::DrawWindow(gc);
+
+	float xMargin = hMargin();
+	float yMargin = vMargin();
+	float x = xMargin;
+	float y = yMargin;
+	float w = Width() - 2.0f * xMargin;
+	float h = Height() - 2.0f * yMargin;
+	if (w <= 0.0f || h <= 0.0f)
+		return;
+
+	UTexture* tex = GetToggle() ? toggleOn() : toggleOff();
+	if (!tex || !normalFont())
+		return;
+
+	if (!bRightSide())
+	{
+		// To do: figure out how checkboxSpacing should be applied
+
+		x += (float)tex->USize();// + checkboxSpacing();
+		if (w - x > 0.0f)
+		{
+			gc->SetAlignments(HAlign(), VAlign());
+			gc->DrawText(x, y, w - x, h, Text());
+		}
+		gc->SetStyle((EDrawStyle)checkboxStyle());
+		gc->SetTileColor(checkboxColor());
+		gc->DrawIcon(xMargin, y + 1.0f /* + (h - (float)tex->VSize()) * 0.5f*/, tex);
+	}
+	else // Note: this is never used as ShowCheckboxOnRightSide is never called
+	{
+		float xExtent = 0.0f, yExtent = 0.0f;
+		gc->GetTextExtent(100000.0f, xExtent, yExtent, Text());
+		gc->SetAlignments(HAlign(), VAlign());
+		gc->DrawText(x, y, w, h, Text());
+		gc->SetStyle((EDrawStyle)checkboxStyle());
+		gc->SetTileColor(checkboxColor());
+		gc->DrawIcon(x + xExtent + checkboxSpacing(), y + 1.0f/* + (h - (float)tex->VSize()) * 0.5f*/, tex);
+	}
+
+	// DrawDebugBox(gc);
+}
+
+void UCheckboxWindow::ParentRequestedPreferredSize(bool bWidthSpecified, float& preferredWidth, bool bHeightSpecified, float& preferredHeight)
+{
+	float xExtent = 0.0f, yExtent = 0.0f;
+	UObject* oldNormalFont = nullptr;
+	UObject* oldBoldFont = nullptr;
+	engine->dxgc->GetFonts(oldNormalFont, oldBoldFont);
+	engine->dxgc->SetFonts(normalFont(), boldFont());
+	engine->dxgc->GetTextExtent(bWidthSpecified ? std::max(preferredWidth, MinWidth()) : 100000.0f, xExtent, yExtent, Text());
+	engine->dxgc->SetFonts(oldNormalFont, oldBoldFont);
+
+	if (UFont* font = normalFont())
+	{
+		int lineHeight = font->GetGlyph('X').VSize;
+		int minHeight = minLines() * lineHeight;
+		int maxHeight = MaxLines() * lineHeight;
+		if (maxHeight > 0 && minHeight <= maxHeight)
+			yExtent = std::clamp(yExtent, (float)minHeight, (float)maxHeight);
+	}
+
+	xExtent = std::max(xExtent, MinWidth());
+
+	if (auto tex = GetToggle() ? toggleOn() : toggleOff())
+	{
+		xExtent += (float)tex->USize() + checkboxSpacing();
+		yExtent = std::max(yExtent, (float)tex->VSize());
+	}
+
+	float xMargin = hMargin();
+	float yMargin = vMargin();
+	if (!bWidthSpecified)
+		preferredWidth = xExtent + xMargin * 2.0f;
+	if (!bHeightSpecified)
+		preferredHeight = yExtent + yMargin * 2.0f;
+
+	UWindow::ParentRequestedPreferredSize(bWidthSpecified, preferredWidth, bHeightSpecified, preferredHeight);
+}
+
+bool UCheckboxWindow::MouseButtonPressed(float pointX, float pointY, EInputKey button, int numClicks)
+{
+	// Should we capture the mouse here?
+	return true;
+}
+
+bool UCheckboxWindow::MouseButtonReleased(float pointX, float pointY, EInputKey button, int numClicks)
+{
+	SetToggle(!GetToggle());
+	for (UWindow* cur = this; cur != nullptr; cur = cur->parentOwner())
+	{
+		if (ToggleChanged(this, GetToggle()))
+			break;
+	}
+	return true;
 }
--- SurrealEngine/Packages/Extension/Windows/Text/UCheckboxWindow.h
+++ SurrealEngine/Packages/Extension/Windows/Text/UCheckboxWindow.h
@@ -13,6 +13,12 @@ public:
 	void SetCheckboxTextures(std::optional<UObject*> toggleOff, std::optional<UObject*> toggleOn, std::optional<float> textureWidth, std::optional<float> textureHeight);
 	void ShowCheckboxOnRightSide(std::optional<bool> bRight);
 
+	void InitWindow() override;
+	void DrawWindow(UGC* gc) override;
+	void ParentRequestedPreferredSize(bool bWidthSpecified, float& preferredWidth, bool bHeightSpecified, float& preferredHeight) override;
+	bool MouseButtonPressed(float pointX, float pointY, EInputKey button, int numClicks) override;
+	bool MouseButtonReleased(float pointX, float pointY, EInputKey button, int numClicks) override;
+
 	BitfieldBool bRightSide() { return BoolValue(PropOffsets_CheckboxWindow.bRightSide); }
 	Color& checkboxColor() { return Value<Color>(PropOffsets_CheckboxWindow.checkboxColor); }
 	float& checkboxSpacing() { return Value<float>(PropOffsets_CheckboxWindow.checkboxSpacing); }
