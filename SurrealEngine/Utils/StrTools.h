1e856803dd2256955318d1e5f0df71cd2f00404f
LupertEverett <86886753+LupertEverett@users.noreply.github.com>
2026-08-05 00:59:09 +0300

    StrTools: Add find_first_of_any()

--- SurrealEngine/Utils/StrTools.h
+++ SurrealEngine/Utils/StrTools.h
@@ -10,4 +10,6 @@ public:
 	static bool startswith(const std::string_view& str, const std::string_view& cmp, bool ignoreCase = false);
 	static std::string replace(const std::string& str, const std::string_view& find, const std::string_view& repl, bool ignoreCase = false);
 	static std::string int_to_string(const int value, const int minWidth);
+	// Finds first occurrence of ANY character given in characters in str. Returns std::string::npos if none of the characters can be found.
+	static size_t find_first_of_any(const std::string& str, const std::string& characters);
 };
